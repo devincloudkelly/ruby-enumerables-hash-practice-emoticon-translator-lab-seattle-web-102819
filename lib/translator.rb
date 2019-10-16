cc4a3fe5f752c2a30ca029401c817b13ee7c5377
Devin Kelly <devin.cloud.kelly@gmail.com>
2019-10-16 20:52:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,6 +27,13 @@ def get_japanese_emoticon(file_path, emoticon)
     end
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, emoticon)
   # code goes here
+  library_hash = load_library(file_path)
+  if library_hash[:get_meaning][emoticon]
+      library_hash[:get_meaning][emoticon]
+    else !library_hash[:get_meaning][emoticon]
+      return "Sorry, that emoticon was not found"
+    end
 end
+
