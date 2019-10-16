14e646017653c6ea9c2316449db609ec086e05ab
Devin Kelly <devin.cloud.kelly@gmail.com>
2019-10-16 20:49:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,8 +22,7 @@ def get_japanese_emoticon(file_path, emoticon)
   library_hash = load_library(file_path)
     if library_hash[:get_emoticon][emoticon]
       library_hash[:get_emoticon][emoticon]
-    end
-    if !library_hash[:get_emoticon][emoticon]
+    else !library_hash[:get_emoticon][emoticon]
       return "Sorry, that isn't an emoticon."
     end
 end
